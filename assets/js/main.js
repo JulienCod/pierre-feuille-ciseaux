bf659997594ecf668a65b2917060a58c52ff699b
Julien Crutain <juju510@hotmail.fr>
2022-01-26 09:29:27 +0100

    ajout du mode 1 joueur 2 joueurs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
+let computer={
+    nom : "",
+    score : 0,
 
+    addScore : (number) =>{
+        return this.score = number;
+    },
+
+    choiceName : ()=>{
+        let tabNom=["Thanos","Spiderman","Thor","Hulk","Captain América","Black Panther","Vision","Iron Man","Docteur Strange",
+        "Batman","Superman","Punisher","Daredevil","Flash","Cyborg","Ant-man","Wolverine"];
+        let random = Math.round(Math.random()*tabNom.length)
+        return this.nom = tabNom[random];
+    }
+}
 
 let playerOne ={
     nom : "",
@@ -24,6 +38,7 @@ let playerTwo ={
 let counter = 1;
 let choicePlayerOne = 0;
 let choicePlayerTwo = 0;
+let number = 0;
 
 //Étape 1 affichage de la page de bienvenue
 
@@ -42,75 +57,134 @@ document.getElementById("game").innerHTML=`
                 A partir de là chaque forme en bat une autre et perd contre une autre. Voici les forces en général au chifumi.<br>
                 Quand vous êtes prêt cliquer sur le boutton <span>"Commencer une partie"</span>
                 </p>
-                <button class="btn" onclick="configGame()">Commencer une partie</button>
+                <button class="btn" onclick="numberPlayer()">Commencer une partie</button>
                 <a href="../../index.html"><button class="btn">Accueil</button></a>
             </div>
         </div>
 `
 
-//Étape 2 aprés un click sur le bouton configuration de la partie
-
-function configGame() {
+//Étape 2 configuration du nombre de joueur.
+function numberPlayer(){
     document.getElementById("game").innerHTML= `
-    <div id="configGame" class="tracking-in-contract-bck-bottom">
+    <div id="configGame1" class="tracking-in-contract-bck-bottom">
+        <div class="flex1">
+            <img src="./assets/images/joueurVSpc.png" class="imgConfig" alt="image d'un smiley qui ne sait pas">
+            <button class="btn" id="choixUnjoueur" onclick="configGameUnJoueur()">1 joueur</button>
+        </div>    
+        <div class ="flex1">
+            <img src="./assets/images/joueurVSjoueur.png" class="imgConfig" alt="image d'un smiley qui ne sait pas">
+            <button class="btn" id="choixdeuxjoueurs" onclick="configGameDeuxJoueurs()">2 joueurs</button>
+        </div>
+    </div>
+    `
+}
+//Étape 3 configuration de la partie pour le mode 1 joueur
+function configGameUnJoueur(){
+    number = 1;
+    computer.nom = computer.choiceName();
+    document.getElementById("game").innerHTML= `
+    <div id="configGame2" class="tracking-in-contract-bck-bottom">
+        <span>Joueur 1</span><br>
+        <label for="namePlayerOne">Nom :</label>
+        <input type="text" name="namePlayerOne" id="namePlayerOne"><br>
+        <label for="namePlayerTwo">Nom :</label>
+        <span>${computer.nom}</span><br>
+        <button class="btn"  onclick="initGame(1)">Valider</button>
+    </div>
+    `
+    document.getElementById("namePlayerOne").focus();
+    document.getElementById("namePlayerOne").addEventListener("keydown",(value)=>{
+        if(value.key === 'Enter'){
+            initGame(1);
+            
+        }
+    })
+}
+//Étape 3 configuration de la partie pour le mode 2 joueurs
+function configGameDeuxJoueurs() {
+    number = 2;
+    document.getElementById("game").innerHTML= `
+    <div id="configGame2" class="tracking-in-contract-bck-bottom">
         <span>Joueur 1</span><br>
         <label for="namePlayerOne">Nom :</label>
         <input type="text" name="namePlayerOne" id="namePlayerOne"><br>
         <span>Joueur 2</span><br>
         <label for="namePlayerTwo">Nom :</label>
         <input type="text" name="namePlayerTwo" id="namePlayerTwo"><br>
-        <button class="btn" id="btn" onclick="initGame()">Valider</button>
+        <button class="btn" onclick="initGame(2)">Valider</button>
     </div>
     `
     document.getElementById("namePlayerOne").focus();
     document.getElementById("namePlayerTwo").addEventListener("keydown",(value)=>{
         if(value.key === 'Enter'){
-            initGame();
+            initGame(2);
+            
         }
     })
 };
 
-//Étape 3 récupération des informations sur les inputs et lancer le jeu
+//Étape 4 récupération des informations sur les inputs et lancement de la partie prend en paramètre le nombre de joueur sélectionné
 
-function initGame(){
-    
-    playerOne.nom = playerOne.configPlayer(document.getElementById("namePlayerOne").value);
-    playerTwo.nom = playerTwo.configPlayer(document.getElementById("namePlayerTwo").value);
-    reset();
+function initGame(number){
+    if(number === 1){
+        playerOne.nom = playerOne.configPlayer(document.getElementById("namePlayerOne").value);
+        reset(1)
+    }else if (number === 2){
+        playerOne.nom = playerOne.configPlayer(document.getElementById("namePlayerOne").value);
+        playerTwo.nom = playerTwo.configPlayer(document.getElementById("namePlayerTwo").value);   
+        reset(2);
+    }
+};
+//Étape 5 initialisation des score et du compteur à zéro
+
+function reset(number) {
+    if(number === 1){
+        playerOne.score = 0;
+        computer.score = 0;
+        counter = 1;
+        choiceOne(1);
+    }else if (number === 2){
+        playerOne.score = 0;
+        playerTwo.score = 0;   
+        counter = 1;
+        choiceOne(2);
+    }
 };
 
-function reset() {
-    playerOne.score = 0;
-    playerTwo.score = 0;
-    counter = 1;
-    choiceOne();
-};
+//Étape 6 choix joueur 1
 
-//Étape 4 choix joueur 1
+function choiceOne(number){
 
-function choiceOne(){
-    
     document.getElementById("game").innerHTML = `
     <div id="playerOne" class="configPlayer tracking-in-contract-bck-bottom">
         <div id="name">${playerOne.nom}</div>
         <div class="win">
-        <h3>Manche gagné : ${playerOne.score}</h3>
-        
+            <h3>Manche gagné : ${playerOne.score}</h3>
         </div>
-        <div class="choice" id="choice" onkeydown="keydown(key)">
-            
-            <input type="image" class="imgChoice" src="./assets/images/pierre.png" value="1" onclick="choiceTwo(value)"  alt="image d'une pierre">
-            <input type="image" class="imgChoice" src="./assets/images/papier.png" value="2" onclick="choiceTwo(value)"  alt="image de papier">
-            <input type="image" class="imgChoice" src="./assets/images/ciseaux.png" value="3" onclick="choiceTwo(value)" alt="image de ciseaux">
+        <div class="choice" id="choice" onkeydown="keydown(key)">`
+        if(number === 1){
+            document.getElementById("choice").innerHTML=`   
+            <input type="image" class="imgChoice" src="./assets/images/pierre.png" value="1" onclick="choiceTwo(value,1)"  alt="image d'une pierre">
+            <input type="image" class="imgChoice" src="./assets/images/papier.png" value="2" onclick="choiceTwo(value,1)"  alt="image de papier">
+            <input type="image" class="imgChoice" src="./assets/images/ciseaux.png" value="3" onclick="choiceTwo(value,1)" alt="image de ciseaux">
         </div>
     </div>`;
+        }else if (number === 2){
+            document.getElementById("choice").innerHTML=`   
+            <input type="image" class="imgChoice" src="./assets/images/pierre.png" value="1" onclick="choiceTwo(value,2)"  alt="image d'une pierre">
+            <input type="image" class="imgChoice" src="./assets/images/papier.png" value="2" onclick="choiceTwo(value,2)"  alt="image de papier">
+            <input type="image" class="imgChoice" src="./assets/images/ciseaux.png" value="3" onclick="choiceTwo(value,2)" alt="image de ciseaux">
+        </div>
+    </div>`; 
+        }
+       
 };
 
-//Étape 5 choix joueur 2
+//Étape 7 choix joueur 2 "joueur ou computer"
 
-function choiceTwo(value){
+function choiceTwo(value,number){
     choicePlayerOne = value
-    if(!isNaN(choicePlayerOne))
+    if(!isNaN(choicePlayerOne) && number === 2){
     document.getElementById("game").innerHTML=`
     <div id="playerTwo" class="configPlayer tracking-in-contract-bck-bottom">
         <div id="name">${playerTwo.nom}</div>
@@ -119,15 +193,32 @@ function choiceTwo(value){
         
         </div>
         <div class="choice">
-            <input type="image" class="imgChoice" src="./assets/images/pierre.png" value="1"  onclick="startGame(value)" alt="image d'une pierre">
-            <input type="image" class="imgChoice" src="./assets/images/papier.png" value="2"  onclick="startGame(value)" alt="image de papier">
-            <input type="image" class="imgChoice" src="./assets/images/ciseaux.png" value="3" onclick="startGame(value)" alt="image de ciseaux">
+            <input type="image" class="imgChoice" src="./assets/images/pierre.png" value="1"  onclick="startGame(value,2)" alt="image d'une pierre">
+            <input type="image" class="imgChoice" src="./assets/images/papier.png" value="2"  onclick="startGame(value,2)" alt="image de papier">
+            <input type="image" class="imgChoice" src="./assets/images/ciseaux.png" value="3" onclick="startGame(value,2)" alt="image de ciseaux">
+        </div>
+
+    </div> `} else if (!isNaN(choicePlayerOne) && number === 1){
+        let value = Math.round(Math.random() * (3 - 1) + 1); 
+        document.getElementById("game").innerHTML=`
+    <div id="playerTwo" class="configPlayer tracking-in-contract-bck-bottom">
+        <div id="name">${computer.nom}</div>
+        <div class="win">
+            <h3>Manche gagné : ${computer.score}</h3>
+        
+        </div>
+        <div class="choice">
+            <input type="image" class="imgChoice" src="./assets/images/pierre.png" value="1" alt="image d'une pierre">
+            <input type="image" class="imgChoice" src="./assets/images/papier.png" value="2" alt="image de papier">
+            <input type="image" class="imgChoice" src="./assets/images/ciseaux.png" value="3" alt="image de ciseaux">
         </div>
 
     </div> `
+    startGame(value);
+    }
 };
 
-//lancement du jeu
+//Étape 8 initialisation de la partie avec les valeurs des joueurs
 
 function startGame(value){
     choicePlayerTwo = value;
@@ -138,7 +229,6 @@ function startGame(value){
             <div id="name">${playerOne.nom}</div>
             <div class="win">
                 <h3>Manche gagné : ${playerOne.score}</h3>
-            
             </div>
             <div class="choice">
                 <input type="image" class="img" src="./assets/images/pierre.png" value="1" alt="image d'une pierre">
@@ -146,6 +236,7 @@ function startGame(value){
                 <input type="image" class="img" src="./assets/images/ciseaux.png" value="3" alt="image de ciseaux">
             </div>
         </div>
+
         <div id="count">
             <div id="manche">
                 <h3>Manche numéro ${counter}</h3>
@@ -155,7 +246,20 @@ function startGame(value){
             </div>
         </div>
 
-        <div id="playerTwo" class="configPlayer">
+        <div id="playerTwo" class="configPlayer">`
+        if(number === 1){
+            document.getElementById("playerTwo").innerHTML = `
+            <div id="name">${computer.nom}</div>
+            <div class="win">
+                <h3>Manche gagné : ${computer.score}</h3>
+            </div>
+            <div class="choice">
+                <input type="image" class="img" src="./assets/images/pierre.png" value="1" alt="image d'une pierre">
+                <input type="image" class="img" src="./assets/images/papier.png" value="2" alt="image de papier">
+                <input type="image" class="img" src="./assets/images/ciseaux.png" value="3" alt="image de ciseaux">
+            </div>`
+        }else if (number === 2){
+            document.getElementById("playerTwo").innerHTML = `
             <div id="name">${playerTwo.nom}</div>
             <div class="win">
                 <h3>Manche gagné : ${playerTwo.score}</h3>
@@ -166,12 +270,12 @@ function startGame(value){
                 <input type="image" class="img" src="./assets/images/papier.png" value="2" alt="image de papier">
                 <input type="image" class="img" src="./assets/images/ciseaux.png" value="3" alt="image de ciseaux">
             </div>
+        
         </div>
-    </div>
-    `
-setTimeout(screenThree,1000);
-};
-
+    </div>`}
+setTimeout(screenThree, 1000);
+}
+//compte à rebours 3
 function screenThree() {
     document.getElementById("count").innerHTML = `
     <div id="manche">
@@ -181,8 +285,9 @@ function screenThree() {
         <span id="timer">3</span>
     </div>
     `
-setTimeout(screenTwo,1000);
+setTimeout(screenTwo, 1000);
 };
+//compte à rebours 2
 function screenTwo() {
     document.getElementById("count").innerHTML = `
     <div id="manche">
@@ -192,8 +297,9 @@ function screenTwo() {
         <span id="timer">2</span>
     </div>
     `
-setTimeout(screenOne,1000);
+setTimeout(screenOne, 1000);
 };
+//compte à rebours1
 function screenOne() {
     document.getElementById("count").innerHTML = `   
     <div id="manche">
@@ -203,8 +309,9 @@ function screenOne() {
         <span id="timer">1</span>
     </div>
     `
-setTimeout(screenZero,1000);
+setTimeout(screenZero, 1000);
 };
+//compte à rebours 0
 function screenZero() {
     document.getElementById("count").innerHTML = `   
     <div id="manche">
@@ -217,14 +324,12 @@ function screenZero() {
 setTimeout(result(choicePlayerOne,choicePlayerTwo),1000);
 };
 
-// fonction qui désigne le vainqueur de la manche
+//Étape 10 fonction qui désigne le vainqueur de la manche
 
 function result(resOne, resTwo) {
 
     choicePlayerOne = parseInt(resOne);
     choicePlayerTwo = parseInt(resTwo);
-    console.log(choicePlayerOne);
-    console.log(choicePlayerTwo);
     // cas d'une égalité
     if(choicePlayerOne === choicePlayerTwo){
         document.getElementById("game").innerHTML = `
@@ -240,7 +345,7 @@ function result(resOne, resTwo) {
             </div>
         </div>
         `
-        setTimeout(suite,5000);
+        setTimeout(suite, 5000);
     }
 
     // tous les cas ou le joueur 1 gagne
@@ -251,10 +356,16 @@ function result(resOne, resTwo) {
 
     // tous les cas ou le joueur 2 gagne
     else if(choicePlayerTwo === 1 && choicePlayerOne ===3 || choicePlayerTwo === 2 && choicePlayerOne === 1 || choicePlayerTwo === 3 && choicePlayerOne === 2){
-        playerTwo.score += playerTwo.addScore(1);
-        screenRes(playerTwo);
+        if(number === 1){
+            computer.score += computer.addScore(1);
+            screenRes(computer);
+        }else if (number === 2){
+            playerTwo.score += playerTwo.addScore(1);
+            screenRes(playerTwo);
+        }
+        
     }
-
+    // affichage du résultat avec  les paramètres du joueur gagnant
     function screenRes(player){
         document.getElementById("game").innerHTML = `
             <div id="screen">  
@@ -270,28 +381,44 @@ function result(resOne, resTwo) {
                 </div>
             </div>
         `
-        setTimeout(suite,5000);
+        setTimeout(suite, 5000);
     }
-
+    // prend tout les paramètres en compte afin de savoir si la partie est terminé ou non
     function suite(){
-
+        // console.log("ici number"+number);  ******Test du nombre de joueur********
         if(counter < 3){
-            if(counter == 2 && playerOne.score == 2 || counter == 2 && playerTwo.score == 2){
-                finish(playerOne, playerTwo);
-            }
-            else{
-                counter++;
-                choiceOne();
-                
+            if(number === 2){
+                if(counter == 2 && playerOne.score == 2 || counter == 2 && playerTwo.score == 2){
+                    finish(playerOne, playerTwo, number);    
+                }else{
+                    counter++;
+                    choiceOne(number);
+                }   
+            } else if (number === 1){
+                if(counter == 2 && playerOne.score == 2 || counter == 2 && computer.score == 2){
+                    finish(playerOne, computer, number);    
+                }else{
+                    counter++;
+                    choiceOne(number);
+                }   
             }
         }
         // tant que le score dès deux joueurs sont égaux et que le 3ieme tour est passé il faut départager les joueurs pour avoir un gagnant.
         else if (counter >= 3){
-            if(playerOne.score === playerTwo.score){
-                counter++;
-                choiceOne();
-            }else{
-                finish(playerOne, playerTwo);
+            if(number === 2){
+                if(playerOne.score === playerTwo.score){
+                    counter++;
+                    choiceOne(number);
+                }else{
+                    finish(playerOne, playerTwo);
+                }    
+            }else if (number === 1){
+                if(playerOne.score === computer.score){
+                    counter++;
+                    choiceOne(number);
+                }else{
+                    finish(playerOne, computer);
+                } 
             }
         }
     }
@@ -325,8 +452,8 @@ function finish(playerone, playertwo) {
                     
                     </p>
                     
-                    <button class="btn" onclick="configGame()">une nouvelle partie</button>
-                    <button class="btn" onclick="reset()">Recommencer une partie</button>
+                    <button class="btn" onclick="numberPlayer()">une nouvelle partie</button>
+                    <button class="btn" onclick="reset(${number})">Recommencer une partie</button>
                     <a href="../../index.html"><button class="btn">Accueil</button></a>
                 </div>
             </div>
@@ -353,8 +480,8 @@ function finish(playerone, playertwo) {
                     
                     </p>
                     
-                    <button class="btn" onclick="configGame()">Une nouvelle partie</button>
-                    <button class="btn" onclick="reset()">Recommencer une partie</button>
+                    <button class="btn" onclick="numberPlayer()">Une nouvelle partie</button>
+                    <button class="btn" onclick="reset(${number})">Recommencer une partie</button>
                     <a href="../../index.html"><button class="btn">Accueil</button></a>
                 </div>
             </div>
